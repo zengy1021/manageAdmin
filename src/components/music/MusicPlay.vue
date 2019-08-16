83d228bf8825b09855ff452f2d5357664b3e6fff
谢超超 <xiechaochao@whzjkj.net>
2019-08-16 18:30:38 +0800

    播放控件

--- src/components/music/MusicPlay.vue
+++ src/components/music/MusicPlay.vue
@@ -8,12 +8,28 @@
       :visible.sync="drawer"
       :direction="direction"
     >
-      <audio :src="src" controls autoplay>你的浏览器不支持audio标签</audio>
+      <audio @canplay="canplay" style="display:none" :src="src" ref="audio" @timeupdate="updateTime" controls autoplay>你的浏览器不支持audio标签</audio>
+      <div class="wrap">
+        <div class="btns">
+          <svg-icon icon-class="last" class="icon"/>
+          <svg-icon :icon-class="hasPlay?'suspend':'play'" class="icon" @click="onPlayType"/>
+          <svg-icon icon-class="next" class="icon"/>
+        </div>
+        <div class="line-wrap">
+          <el-progress ref="progress" :percentage="percentage" :show-text="false" type="line" class="line"></el-progress>
+          <svg-icon v-drag="" icon-class="circle" :style="{left:circleLeft+'px'}" class="icon-circle"/>
+          <span class="time">{{currentTime | formatDuring}}/{{totalTime | formatDuring}}</span>
+        </div>
+        <div class="other">
+          
+        </div>
+      </div>
     </el-drawer>
     <!-- <audio :src="src" controls autoplay>你的浏览器不支持audio标签</audio> -->
   </div>
 </template>
 <script>
+let $this;
 export default {
   name: 'MusicPlay',
   props: {
@@ -29,8 +45,16 @@ export default {
   data() {
     return {
       drawer: false,
-      direction: 'btt'
+      direction: 'btt',
+      hasPlay:false,
+      currentTime:0,
+      totalTime:0,
+      percentage:0,
+      circleLeft:0
     }
+  },
+  created(){
+    $this = this;
   },
   watch: {
     isOpen(val) {
@@ -42,12 +66,115 @@ export default {
       if (!val) {
         this.$store.dispatch('music/closeDrawer')
       }
-    }
+    },
   },
   methods: {
+    updateTime(e){
+      this.currentTime = e.target.currentTime;
+      const width = this.$refs.progress.$el.offsetWidth;
+      const scale = parseInt(e.target.currentTime) / parseInt(e.target.duration);
+      this.percentage = scale*100;
+      this.circleLeft = scale*width-8;
+      if(this.percentage == 100){
+        this.hasPlay = false;
+      }
+    },
+    onPlayType(){
+      if(this.hasPlay){
+        this.$refs.audio.pause();
+      }else{
+        this.$refs.audio.play();
+      }
+      this.hasPlay = !this.hasPlay;
+    },
     drawerBtn() {
       this.drawer = !this.drawer
+    },
+    canplay(e){
+      this.hasPlay = true;
+      this.totalTime = e.target.duration;
+    },
+  },
+  filters:{
+    formatDuring(mss) {
+        var minutes = parseInt((parseInt(mss) % (60 * 60)) / 60);
+        var seconds = parseInt(parseInt(mss) % 60);
+        let str = '';
+        str += minutes?minutes+ ":":'0' + ":";
+        str += seconds?seconds>10?seconds:'0'+seconds:'00';
+        return str;
+    },
+  },
+  directives:{
+    drag(el, bindings){
+      if(!$this.totalTime) return;
+        el.onmousedown = function(e){
+            const width = e.path[2].offsetWidth;
+            const widthLeft = e.path[2].offsetLeft;
+            document.onmousemove = function (e){
+                if(e.pageX - widthLeft <= 0){
+                  $this.circleLeft = 0;
+                }else if(e.pageX - widthLeft >= width){
+                  $this.circleLeft = width-5;
+                }else{
+                  $this.circleLeft = e.pageX - widthLeft-5;
+                }
+                const scale = $this.circleLeft/width;
+                $this.percentage = 100*scale;
+            }
+            document.onmouseup = function(){
+              document.onmousemove = document.onmouseup = null;
+            }
+        }
     }
-  }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+    .wrap{
+      width: 80%;
+      margin: auto;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      -o-user-select: none;
+      -moz-user-select: none; /*火狐 firefox*/
+      -webkit-user-select: none; /*webkit浏览器*/
+      -ms-user-select: none; /*IE10+*/
+      -khtml-user-select :none; /*早期的浏览器*/
+      user-select: none;
+      .btns{
+        .icon{
+          font-size: 32px;
+          color: #999;
+          cursor: pointer;
+        }
+        .icon:hover{
+          color: #1AAD19;
+        }
+      }
+      .line-wrap{
+        flex: 1;
+        margin:0 15px;
+        position: relative;
+        .icon-circle{
+          position: absolute;
+          left: 0;
+          top: -5px;
+          font-size: 16px;
+          color: #409eff;
+          cursor: pointer;
+        }
+        .time{
+          position: absolute;
+          top: 10px;
+        }
+      }
+      .other{
+        width: 50px;
+        background: #1AAD19;
+        height: 32px;
+      }
+    }
+</style>
