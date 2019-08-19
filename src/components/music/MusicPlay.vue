ca9cc4be3077c13858a18af5fb0c0c259eab2364
18671561774@163.com <18671561774@163.com>
2019-08-19 10:00:52 +0800

    README-zh.md里面有自动编译eslint

--- src/components/music/MusicPlay.vue
+++ src/components/music/MusicPlay.vue
@@ -8,30 +8,61 @@
       :visible.sync="drawer"
       :direction="direction"
     >
-      <audio @canplay="canplay" style="display:none" :src="src" ref="audio" @timeupdate="updateTime" controls autoplay>你的浏览器不支持audio标签</audio>
+      <audio ref="audio" style="display:none" :src="src" controls autoplay @canplay="canplay" @timeupdate="updateTime">你的浏览器不支持audio标签</audio>
       <div class="wrap">
         <div class="btns">
-          <svg-icon icon-class="last" class="icon"/>
-          <svg-icon :icon-class="hasPlay?'suspend':'play'" class="icon" @click="onPlayType"/>
-          <svg-icon icon-class="next" class="icon"/>
+          <svg-icon icon-class="last" class="icon" />
+          <svg-icon :icon-class="hasPlay?'suspend':'play'" class="icon" @click="onPlayType" />
+          <svg-icon icon-class="next" class="icon" />
         </div>
         <div class="line-wrap">
-          <el-progress ref="progress" :percentage="percentage" :show-text="false" type="line" class="line"></el-progress>
-          <svg-icon v-drag="" icon-class="circle" :style="{left:circleLeft+'px'}" class="icon-circle"/>
-          <span class="time">{{currentTime | formatDuring}}/{{totalTime | formatDuring}}</span>
+          <el-progress ref="progress" :percentage="percentage" :show-text="false" type="line" class="line" />
+          <svg-icon v-drag="" icon-class="circle" :style="{left:circleLeft+'px'}" class="icon-circle" />
+          <span class="time">{{ currentTime | formatDuring }}/{{ totalTime | formatDuring }}</span>
         </div>
-        <div class="other">
-          
-        </div>
+        <div class="other" />
       </div>
     </el-drawer>
     <!-- <audio :src="src" controls autoplay>你的浏览器不支持audio标签</audio> -->
   </div>
 </template>
 <script>
-let $this;
+let $this
 export default {
   name: 'MusicPlay',
+  filters: {
+    formatDuring(mss) {
+      var minutes = parseInt((parseInt(mss) % (60 * 60)) / 60)
+      var seconds = parseInt(parseInt(mss) % 60)
+      let str = ''
+      str += minutes ? minutes + ':' : '0' + ':'
+      str += seconds ? seconds > 10 ? seconds : '0' + seconds : '00'
+      return str
+    }
+  },
+  directives: {
+    drag(el, bindings) {
+      if (!$this.totalTime) return
+      el.onmousedown = function(e) {
+        const width = e.path[2].offsetWidth
+        const widthLeft = e.path[2].offsetLeft
+        document.onmousemove = function(e) {
+          if (e.pageX - widthLeft <= 0) {
+            $this.circleLeft = 0
+          } else if (e.pageX - widthLeft >= width) {
+            $this.circleLeft = width - 5
+          } else {
+            $this.circleLeft = e.pageX - widthLeft - 5
+          }
+          const scale = $this.circleLeft / width
+          $this.percentage = 100 * scale
+        }
+        document.onmouseup = function() {
+          document.onmousemove = document.onmouseup = null
+        }
+      }
+    }
+  },
   props: {
     src: {
       type: String,
@@ -46,15 +77,12 @@ export default {
     return {
       drawer: false,
       direction: 'btt',
-      hasPlay:false,
-      currentTime:0,
-      totalTime:0,
-      percentage:0,
-      circleLeft:0
+      hasPlay: false,
+      currentTime: 0,
+      totalTime: 0,
+      percentage: 0,
+      circleLeft: 0
     }
-  },
-  created(){
-    $this = this;
   },
   watch: {
     isOpen(val) {
@@ -66,68 +94,38 @@ export default {
       if (!val) {
         this.$store.dispatch('music/closeDrawer')
       }
-    },
+    }
+  },
+  created() {
+    $this = this
   },
   methods: {
-    updateTime(e){
-      this.currentTime = e.target.currentTime;
-      const width = this.$refs.progress.$el.offsetWidth;
-      const scale = parseInt(e.target.currentTime) / parseInt(e.target.duration);
-      this.percentage = scale*100;
-      this.circleLeft = scale*width-8;
-      if(this.percentage == 100){
-        this.hasPlay = false;
+    updateTime(e) {
+      this.currentTime = e.target.currentTime
+      const width = this.$refs.progress.$el.offsetWidth
+      const scale = parseInt(e.target.currentTime) / parseInt(e.target.duration)
+      this.percentage = scale * 100
+      this.circleLeft = scale * width - 8
+      if (this.percentage === 100) {
+        this.hasPlay = false
       }
     },
-    onPlayType(){
-      if(this.hasPlay){
-        this.$refs.audio.pause();
-      }else{
-        this.$refs.audio.play();
+    onPlayType() {
+      if (this.hasPlay) {
+        this.$refs.audio.pause()
+      } else {
+        this.$refs.audio.play()
       }
-      this.hasPlay = !this.hasPlay;
+      this.hasPlay = !this.hasPlay
     },
     drawerBtn() {
       this.drawer = !this.drawer
     },
-    canplay(e){
-      this.hasPlay = true;
-      this.totalTime = e.target.duration;
-    },
-  },
-  filters:{
-    formatDuring(mss) {
-        var minutes = parseInt((parseInt(mss) % (60 * 60)) / 60);
-        var seconds = parseInt(parseInt(mss) % 60);
-        let str = '';
-        str += minutes?minutes+ ":":'0' + ":";
-        str += seconds?seconds>10?seconds:'0'+seconds:'00';
-        return str;
-    },
-  },
-  directives:{
-    drag(el, bindings){
-      if(!$this.totalTime) return;
-        el.onmousedown = function(e){
-            const width = e.path[2].offsetWidth;
-            const widthLeft = e.path[2].offsetLeft;
-            document.onmousemove = function (e){
-                if(e.pageX - widthLeft <= 0){
-                  $this.circleLeft = 0;
-                }else if(e.pageX - widthLeft >= width){
-                  $this.circleLeft = width-5;
-                }else{
-                  $this.circleLeft = e.pageX - widthLeft-5;
-                }
-                const scale = $this.circleLeft/width;
-                $this.percentage = 100*scale;
-            }
-            document.onmouseup = function(){
-              document.onmousemove = document.onmouseup = null;
-            }
-        }
+    canplay(e) {
+      this.hasPlay = true
+      this.totalTime = e.target.duration
     }
-  },
+  }
 }
 </script>
 
